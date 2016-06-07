8b2c5e4b117497937a19f1ad13b302a47941f93f
Katrina Tollin <ktollin@gmail.com>
2016-06-06 19:45:55 -0500

    Update alias file with a loop

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,15 +1,24 @@
-p "Welcome, please state your first and last name."
-  real_name = gets.chomp
 
-# reverse order of first and last name
-real_name = real_name.split(' ').reverse
-# separate letters into array
-real_name = real_name.join.split('')
-# change each letter to next in alphabet
-real_name.map! {|letter| letter.next }
-# join letter together
-real_name = real_name.join('')
+input = ''
+while input != "quit"
+    p "To continue with the name-generating process, press ENTER. To exit program, type 'quit.'"
+      input = gets.chomp
+      break if input == "quit"
+    p "What is the name we will be processing?"
+    name = gets.chomp
 
+    def make_alias (name)
+      # reverse order of first and last name
+      name = name.split(' ').reverse
+      # separate letters into array
+      name = name.join.split('')
+      # change each letter to next in alphabet
+      name.map! {|letter| letter.next }
+      # join letter together
+      name = name.join('')
+    end
+    p "Great. your new name is #{make_alias(name)}."
 
-p "Great. your new name is #{real_name}."
+end
+
 
