fa7e6de4499db8612540adcfdb32b1329c3b79e1
Katrina Tollin <ktollin@gmail.com>
2016-06-09 16:54:01 -0500

    Add more attribute-changing methods and refractor

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,6 +1,8 @@
 
 
 class Santa
+  attr_reader :ethnicity
+  attr_accessor :gender, :reindeer_ranking, :age
 
   def initialize (gender, ethnicity)
     puts "Initializing Santa instance.."
@@ -19,19 +21,46 @@ class Santa
     puts "That was a good #{type} cookie!"
   end
 
+  def celebrate_birthday
+    @age += 1
+    p "It's a birthday! This Santa is now #{@age}-years-old!"
+  end
+
+  def get_mad_at(reindeer_name)
+    if @reindeer_ranking.include?(reindeer_name)
+      @reindeer_ranking.delete(reindeer_name)
+      @reindeer_ranking.push(reindeer_name)
+      puts "The new ranking is: #{@reindeer_ranking}"
+    else
+      puts "Hmm.. that is not a registered reindeer."
+    end
+  end
+
 end
 
 santas = []
 example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
 example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
-example_genders.length.times do |i|
-  santas << Santa.new(example_genders[i], example_ethnicities[i])
+example_genders.length.times do |num|
+  santas << Santa.new(example_genders[num], example_ethnicities[num])
 end
 
+# DRIVER CODE
+
+# p santas[0].celebrate_birthday
+wayne = Santa.new("gender fluid", "elvin")
+donald = Santa.new("non-binary", "troll")
+# donald.celebrate_birthday
+# donald.get_mad_ad("Dasher")
+# p donald.reindeer_ranking
+p wayne.gender = "male"
+
+# sammy = Santa.new("male", "latino")
+# sammy.change_gender("unidentified")
+# p sammy.ethnicity
 
 
-# Wayne = Santa.new("gender fluid", "elvin")
-
+# ALTERNATE ARRAY METHOD
 # santas = []
 # santas << Santa.new("agender", "black")
 # santas << Santa.new("female", "Latino")
@@ -40,5 +69,6 @@ end
 # santas << Santa.new("female", "prefer not to say")
 # santas << Santa.new("gender fluid", "Mystical Creature (unicorn)")
 # santas << Santa.new("N/A", "N/A")
+
 
 
