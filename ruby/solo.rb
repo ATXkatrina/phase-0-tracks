1aad27999b99ee9feb22724752f14e856a994d7f
Katrina Tollin <ktollin@gmail.com>
2016-06-12 16:41:05 -0500

    Add to user interface

--- ruby/solo.rb
+++ ruby/solo.rb
@@ -12,14 +12,14 @@ class One_ring
 attr_reader :inscription
 attr_accessor :character_with_ring, :utilized_power, :destined_for_Mount_Doom, :name
 
-  def initialize(new_ring, given_character, power)
+  def initialize(new_ring, given_character, power, mount_doom)
     puts "Initializing the One Ring.."
     @name = new_ring
     @character_with_ring = given_character
-    @destined_for_Mount_Doom = true
+    @destined_for_Mount_Doom = nil
     @utilized_power = power
     @inscription = "Ash nazg durbatuluk, ash nazg gimbatul,
-Ash nazg thrakatuluk agh burzum-ishi krimpatul."
+      Ash nazg thrakatuluk agh burzum-ishi krimpatul."
   end
 
   def current_holder(given_character)
@@ -49,8 +49,21 @@ Ash nazg thrakatuluk agh burzum-ishi krimpatul."
     end
   end
 
-end
+  def destination(mount_doom)
+    destined_for_Mount_Doom = mount_doom
+    if mount_doom == "y"
+      mount_doom = true
+      puts "The ring is being taken to Mount Doom for destruction"
+    elsif mount_doom == "n"
+      mount_doom = false
+      puts "The ring can be intercepted by Sauron for evil! It must be destroyed!"
+    else
+      puts "Try entering 'y' or 'n' next time."
+    end
 
+  end
+
+end
 
 new_ring = ""
 ring_history = []
@@ -62,11 +75,15 @@ while new_ring != "quit"
   given_character = gets.chomp
   p "What power is the ring currently exerting? Choose: 'invisibility,' 'will domination,' 'invincibility,' or 'mind reading' - or 'random.'"
   power = gets.chomp
-  new_ring = One_ring.new(new_ring, given_character, power)
+  p "Are we taking the ring to Mount Doom for destruction? (y/n)"
+    mount_doom = gets.chomp
+
+  new_ring = One_ring.new(new_ring, given_character, power, mount_doom)
   new_ring.current_holder(given_character)
   new_ring.show_inscription
   new_ring.seek_other_rings
   new_ring.exert_power(power)
+  new_ring.destination(mount_doom)
   ring_history << new_ring
 end
 p ring_history
